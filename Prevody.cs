4f8cb59014c00762247254cc4abb0afaf0fcc0a0
bodnarb.04 <bodnarb.04@A321-13>
2022-06-13 13:12:50 +0200

    BinToDec

---
+++ Prevody.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiselneSoustavy
+{
+    class Prevody
+    {
+        public string BinToDec(int bin)
+        {
+            double dec = 0;
+            for (int i = 0; bin > 0; i++)
+            {
+                int cislo = bin % 10;
+                if(cislo == 1)
+                {
+                    dec += Math.Pow(2, i);
+                }
+                bin = bin / 10;
+            }
+
+            return dec.ToString();
+        }
+
+
+    }
+}
